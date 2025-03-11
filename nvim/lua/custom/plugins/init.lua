2154f2456f84573b139446863334e1afa0877730
VLvin <ghostv.work@gmail.com>
2025-03-11 22:13:59 +0400

    All I need is...

---
+++ nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,11 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  require 'custom.plugins.oil',
+  require 'custom.plugins.flash',
+  require 'custom.plugins.md-preview',
+  require 'custom.plugins.catppuccin',
+  require 'custom.plugins.vim-multi-cursors',
+}
